3fef97abd8b51b1efc70c3a3c70d99fbc599a6a7
Stew Heckenberg <stewart.heckenberg@gmail.com>
2018-10-03 17:52:58 +1000

    npm i -s sass-loader node-sass pug pug-plain-loader buefy

--- .vuepress/theme/Layout.vue
+++ .vuepress/theme/Layout.vue
@@ -4,6 +4,21 @@
     Content
 </template>
 
+<!--
+<script lang="coffee">
+import Vue from 'vue'
+import Buefy from 'buefy'
+# import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
+
+export default {
+  data: ->
+    return foo: 'bar'
+}
+</script>
+-->
+
 <script>
 import Vue from 'vue'
 import Buefy from 'buefy'
